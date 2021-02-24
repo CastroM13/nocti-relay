1d4dcd574ecb62d7331992f793b7ead7ae3d4ab0
Matheus Notebook <matheusbjorn@gmail.com>
2021-02-24 12:12:12 -0300

    port correction

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var express = require("express");
 var app = express();
 
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running on port ", process.env.PORT || 3000);
 });
 
 app.use(express.json())
