c376039c59814f47cfd2a492d3af5a69d7ee6d79
Matheus Notebook <matheusbjorn@gmail.com>
2021-02-25 22:39:18 -0300

    'redeploy'

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const connStr = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:baserelay.database.windows.net,1433;Database=Project Aria;Uid=castrom13;Pwd=5398Samanco@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
+const connStr = "Server=baserelay.database.windows.net;Database=Project Aria;User Id=castrom13;Password=5398Samanco@; Encrypt=true";
 const sql = require("mssql");
-var _ = require("lodash");
 var express = require("express");
 
 var app = express();
@@ -9,44 +8,27 @@ app.listen(process.env.PORT || 3000, process.env.YOUR_HOST || "0.0.0.0", () => {
   console.log("Server running on port ", process.env.PORT || 3000);
 });
 
-app.use(express.json());
+// app.use(express.json());
 
-app.post("user/:id/characters", (req, res, next) => {
+app.get("/", (req, res, next) => {
+  const id = req.params.id;
   sql
     .connect(connStr)
     .then(async function retorno(conn) {
-      res.json(conn);
-    //   const result = await sql.query`SELECT * FROM fn_showUserCharacters(${req.params.id})`;
-    //   res.json(result);
+      const result = await sql.query`SELECT * FROM fn_showUserCharacters(${id})`;
+      res.json(result);
+    })
+    .catch((err) => console.log("erro! " + err));
+  // res.json("sucesso");
+});
+
+app.get("/user/:id/characters", (req, res, next) => {
+  const id = req.params.id;
+  sql
+    .connect(connStr)
+    .then(async function retorno(conn) {
+      const result = await sql.query`SELECT * FROM fn_showUserCharacters(${id})`;
+      res.json(result);
     })
     .catch((err) => console.log("erro! " + err));
 });
-
-// app.post("/players", (req, res, next) => {
-//   const value = req.body;
-//   if (value !== "") {
-//     sql
-//       .connect(connStr)
-//       .then(async function retorno(conn) {
-//         const result = await sql.query`INSERT INTO players(Player_Name) VALUES(${value.Player_Name})`;
-//         res.json(result);
-//       })
-//       .catch((err) => console.log("Erro! " + err));
-//   } else {
-//     res.json({ result: "Erro! A Inserção não pode estar vazia." });
-//   }
-// });
-// app.post("/login", (req, res, next) => {
-//   const value = req.body;
-//   if (value !== "") {
-//     sql
-//       .connect(connStr)
-//       .then(async function retorno(conn) {
-//         const result = await sql.query`INSERT INTO players(Player_Name) VALUES(${value.Player_Name})`;
-//         res.json(result);
-//       })
-//       .catch((err) => console.log("Erro! " + err));
-//   } else {
-//     res.json({ result: "Erro! A Inserção não pode estar vazia." });
-//   }
-// });
