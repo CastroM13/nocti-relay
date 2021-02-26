a56988a8f9c1c8efdd0658b31edf02535d152dd0
Matheus Notebook <matheusbjorn@gmail.com>
2021-02-25 22:47:12 -0300

    'redeploy'

--- index.js
+++ index.js
@@ -11,15 +11,7 @@ app.listen(process.env.PORT || 3000, process.env.YOUR_HOST || "0.0.0.0", () => {
 // app.use(express.json());
 
 app.get("/", (req, res, next) => {
-  const id = req.params.id;
-  sql
-    .connect(connStr)
-    .then(async function retorno(conn) {
-      const result = await sql.query`SELECT * FROM fn_showUserCharacters(${id})`;
-      res.json(result);
-    })
-    .catch((err) => console.log("erro! " + err));
-  // res.json("sucesso");
+  res.send(<div>Sucesso!</div>)
 });
 
 app.get("/user/:id/characters", (req, res, next) => {
@@ -32,3 +24,14 @@ app.get("/user/:id/characters", (req, res, next) => {
     })
     .catch((err) => console.log("erro! " + err));
 });
+
+app.post("login", (req, res, next) => {
+    const value = req.body;
+    sql
+      .connect(connStr)
+      .then(async function retorno(conn) {
+        const result = await sql.query`SELECT * FROM fn_login(${value.Username},${value.Password})`;
+        res.json(result);
+      })
+      .catch((err) => console.log("erro! " + err));
+})
